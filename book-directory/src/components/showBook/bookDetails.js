cf4804a5041081f6c2363dff1ccfa99d98d17156
ommiy2j <ommiy2j@gmail.com>
2021-04-13 15:38:26 +0530

    initial commit

--- book-directory/src/components/showBook/bookDetails.js
+++ book-directory/src/components/showBook/bookDetails.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import book from '../../../../model/book';
 import fakePlaceholder from '../../assets/books/fakeBook.png';
-
-
 
 function BookDetails (props) {
 	const bookId = props.match.params.bookId;
-    const [bookDetails, setbookDetails] = useState({});
-    const [image, setImage] = useState('');
+	const [ bookDetails, setbookDetails ] = useState({});
+	const [ image, setImage ] = useState('');
 	const showDetails = () => {
 		fetch(`https://bookshelf124.herokuapp.com/book/showbooks/bookdetails/${bookId}`, {
 			headers: {
@@ -18,26 +17,28 @@ function BookDetails (props) {
 			})
 			.then((result) => {
 				console.log(result);
-                setbookDetails(result.book);
-                setImage(result.book.image)
+				setbookDetails(result.book);
+				setImage(result.book.image);
 			});
 	};
 	useEffect(() => {
-        showDetails();
-        console.log(bookDetails);
-	},[]);
+		showDetails();
+	}, []);
 	return (
 		<div>
 			<div className='book-details-container'>
 				<div className='book-image2'>
-                <img src={image ? `https://bookshelf124.herokuapp.com${image}` : fakePlaceholder} alt='' />
-                </div>
-                <div className='book-details' >
-                    <div className="book-head">
-                        <h2>{bookDetails.name}</h2>
-                        <h6>By-{ bookDetails.author }</h6>
-                    </div>
-                </div>
+					<img src={image ? `https://bookshelf124.herokuapp.com${image}` : fakePlaceholder} alt='' />
+				</div>
+				<div className='book-details'>
+					<div className='book-head'>
+						<h2>{bookDetails.name}</h2>
+						<h6>By-{bookDetails.author}</h6>
+					</div>
+				</div>
+				<div className='book-desc'>
+					<p>{bookDetails.details}</p>
+				</div>
 			</div>
 		</div>
 	);
