52f6a0a3410de9253b5854e0a75cea785efe188c
ommiy2j <ommiy2j@gmail.com>
2021-04-14 11:58:20 +0530

    initial commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	next();
 });
-app.use(helmet());
+
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		cb(null, 'images');
